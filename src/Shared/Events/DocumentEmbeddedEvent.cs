5bee5906680d2bc95f979209a0d2e03172b1921d
ThanosM97 <thanosmas97@gmail.com>
2025-02-15 17:18:13 +0000

    Add DocumentEmbeddedEvent

---
+++ src/Shared/Events/DocumentEmbeddedEvent.cs
@@ -0,0 +1,31 @@
+using Shared.Models;
+
+namespace Shared.Events
+{
+    /// <summary>
+    /// Represents an event that is triggered when a document is chunked into smaller parts.
+    /// </summary>
+    /// <param name="DocumentId">The unique identifier for the document that has been chunked.</param>
+    /// <param name="ObjectStorageKey">
+    ///      The key or path in the object storage system to locate the original document or its representation.
+    ///  </param>
+    /// <param name="FileName">The name of the document file that was parsed.</param>
+    /// <param name="Chunks">
+    ///      A list of <see cref="DocumentChunk"/> objects representing the individual parts
+    ///      into which the document has been divided.
+    /// </param>
+    /// <param name="Embeddings">
+    ///      An array of embedding vectors of type float, associated with the chunks.
+    /// </param>
+    /// <param name="EmbeddedAt">
+    ///      The date and time when the document was embedded.
+    ///  </param>
+    public record DocumentEmbeddedEvent(
+        string DocumentId,
+        string ObjectStorageKey,
+        string FileName,
+        List<DocumentChunk> Chunks,
+        float [][] Embeddings,
+        DateTime EmbeddedAt
+    );
+}

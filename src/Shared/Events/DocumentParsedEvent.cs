6d9108f473804869193c9ee20777375470c783c0
ThanosM97 <thanosmas97@gmail.com>
2025-02-24 20:55:09 +0000

    Remove trailing whitespaces

--- src/Shared/Events/DocumentParsedEvent.cs
+++ src/Shared/Events/DocumentParsedEvent.cs
@@ -1,12 +1,12 @@
 namespace Shared.Events
-{    
-    /// <summary>  
-    /// Represents an event that is triggered when a document has been parsed.  
-    /// </summary>  
-    /// <param name="DocumentId">The unique identifier of the document.</param>  
-    /// <param name="ObjectStorageKey">The key used to locate the document in object storage.</param>  
-    /// <param name="FileName">The name of the document file that was parsed.</param>  
-    /// <param name="ParsedAt">The date and time when the document was parsed.</param>  
+{
+    /// <summary>
+    /// Represents an event that is triggered when a document has been parsed.
+    /// </summary>
+    /// <param name="DocumentId">The unique identifier of the document.</param>
+    /// <param name="ObjectStorageKey">The key used to locate the document in object storage.</param>
+    /// <param name="FileName">The name of the document file that was parsed.</param>
+    /// <param name="ParsedAt">The date and time when the document was parsed.</param>
     /// <param name="ParsedTextContent">The text content extracted from the parsed document.</param>
     public record DocumentParsedEvent(
         string DocumentId,

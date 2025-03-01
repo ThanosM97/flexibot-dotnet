e44da05018727813e8318c01b122effd9f4d1bd7
ThanosM97 <thanosmas97@gmail.com>
2025-03-01 10:38:13 +0000

    Add exchange parameter to base rabbitmq consumer

--- src/Shared/Services/RabbitMQConsumerBase.cs
+++ src/Shared/Services/RabbitMQConsumerBase.cs
@@ -16,21 +16,32 @@ namespace Shared.Services
         protected readonly IConnection Connection;
         protected readonly IChannel Channel;
         protected readonly string QueueName;
+        protected readonly string Exchange;
 
-        protected RabbitMQConsumerBase(IConnection connection, string queueName)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RabbitMQConsumerBase"/> class.
+        /// </summary>
+        /// <param name="connection">The RabbitMQ connection.</param>
+        /// <param name="queueName">The name of the queue to consume from.</param>
+        /// <param name="exchange">The name of the exchange to bind the queue to. Defaults to "documents".</param>
+        protected RabbitMQConsumerBase(IConnection connection, string queueName, string exchange = "documents")
         {
+            // Initialize the RabbitMQ connection and channel
             Connection = connection;
             Channel = connection.CreateChannelAsync().Result;
+
+            // Set the queue and exchange names
             QueueName = queueName;
+            Exchange = exchange;
 
             // Declare the exchange
-            Channel.ExchangeDeclareAsync(exchange: "documents", type: ExchangeType.Direct, durable: true);
+            Channel.ExchangeDeclareAsync(exchange: Exchange, type: ExchangeType.Direct, durable: true);
 
             // Declare the queue
             Channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false);
 
             // Bind the queue to the exchange
-            Channel.QueueBindAsync(queue: queueName, exchange: "documents", routingKey: queueName);
+            Channel.QueueBindAsync(queue: queueName, exchange: Exchange, routingKey: queueName);
         }
 
         /// <summary>
@@ -71,7 +82,7 @@ namespace Shared.Services
 
             // Publish the event to the specified exchange and routing key
             await Channel.BasicPublishAsync(
-                exchange: "documents",
+                exchange: Exchange,
                 routingKey: "document_status",
                 body: JsonSerializer.SerializeToUtf8Bytes(statusEvent)
             );
